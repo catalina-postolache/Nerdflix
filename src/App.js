0441bcb2dfe9fa4467570e31ca5d29920f241a13
catalina1505 <catalina150592@gmail.com>
2019-03-23 16:13:52 +0100

    bind json data to component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import Movies from './Movies.js';
 import './App.css';
+import {
+  Typography,
+  Grid,
+  Button,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  TextField,
+  Card
+} from '@material-ui/core'; 
+
+import data from './imdb-top-50.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { data }
+    this.sortByPriceAsc = this.sortByPriceAsc.bind(this);
+    this.sortByPriceDesc = this.sortByPriceDesc.bind(this);
+  };
+
+  sortByPriceAsc() {
+    this.setState(prevState => {
+      this.state.data.data.movies.sort((a, b) => (a.rating - b.rating))
+      console.log(data.data.movies[0].rating)
+  });
+  }
+
+  sortByPriceDesc() {
+    this.setState(prevState => {
+      this.state.data.data.movies.sort((a, b) => (b.rating - a.rating))
+  });
+  } 
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Grid>
+            <Button onClick={this.sortByPriceAsc}>
+          ASC
+        </Button>
+        <Button onClick={this.sortByPriceDesc}>
+          DESC
+        </Button>
+
+            {data.data.movies.map((el, index) => 
+                <Card>
+                    <img src={el.urlPoster} alt='no pic'/>
+                    <Typography >                        
+                        {el.title}
+                    </Typography>
+                    <Typography >
+                        {el.year}
+                    </Typography>               
+            </Card>
+            )} 
+        </Grid>
     );
   }
 }
